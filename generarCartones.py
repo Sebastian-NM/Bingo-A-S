bd02d46abe4fe2b5c75b9a9be36bcdf5730b4f2e
Sebastian-NM <82008773+Sebastian-NM@users.noreply.github.com>
2021-11-20 13:52:29 -0600

    Listo generar coartones 2

--- generarCartones.py
+++ generarCartones.py
@@ -1,3 +1,8 @@
+from tkinter import *
+import menuPrincipal as MP
+import logica as logic
+
+
 '''
 Proyecto Programado 02
 Tema: Gestor de Bingos
@@ -5,18 +10,10 @@ Estudiantes: Angela González Solano, 2021445876
              Sebastián Navarro Martinez, 2021579550
 '''
 
-
-#Importar Librerías
-from tkinter import *
-import menuPrincipal as MP
-import logica as logic
-
-
-#Inicio variables globales
-generarCartonesPantalla=""
-label3=""
-cantidadCartones=0
-
+winGenerarCartones=""
+lblInfoError=""
+ntryCantidadCartones=0
+ 
 
 '''
 Entradas: cantidadCartonesText
@@ -26,38 +23,40 @@ Restriciones: cantidadCartonesText (str)
 '''
 #genera los cartones para la partida
 def generarFuncion():
-    global generarCartonesPantalla
-    global cantidadCartones
-    global label3
+    global winGenerarCartones
+    global ntryCantidadCartones
+    global lblInfoError
 
-    cantidadCartonesText = cantidadCartones.get();
 
-    if(cantidadCartonesText!="" and cantidadCartonesText!="Valor entre 1 y 500"):
-        if(cantidadCartonesText.isnumeric()):
-            cantidadCartonesText = int(cantidadCartonesText)
-            if(cantidadCartonesText>=1 and cantidadCartonesText<=500 ):
-                logic.imprimirMatrices(cantidadCartonesText)
+    ntryCantidadCartonesGetter = ntryCantidadCartones.get()
+
+    if(ntryCantidadCartonesGetter!=""):
+        if(ntryCantidadCartonesGetter.isnumeric()):
+            ntryCantidadCartonesGetter = int(ntryCantidadCartonesGetter)
+            if(ntryCantidadCartonesGetter>=1 and ntryCantidadCartonesGetter<=500 ):
+                logic.imprimirMatrices(ntryCantidadCartonesGetter)
                 
-                if(cantidadCartonesText==1):
-                    label3.destroy()
-                    label3=Label(generarCartonesPantalla,text="Se creó "+str(cantidadCartonesText)+" cartón", bg="#B0E0E6", fg="black", font=("Finland", 10, 'bold'))
-                    label3.place(x=250,y=40)                   
+                if(ntryCantidadCartonesGetter==1):
+                    lblInfoError.destroy()
+                    lblInfoError=Label(winGenerarCartones,text="Se creó "+str(ntryCantidadCartonesGetter)+" cartón", bg="#B0E0E6", font=("Finland", 10, 'bold'), fg ="#0EA928")
+                    lblInfoError.place(x=200,y=120)                   
                 else:
-                    label3.destroy()
-                    label3=Label(generarCartonesPantalla,text="Se crearon "+str(cantidadCartonesText)+" cartones", bg="#B0E0E6", fg="black", font=("Finland", 10, 'bold'))
-                    label3.place(x=230,y=40)
+                    lblInfoError.destroy()
+                    lblInfoError=Label(winGenerarCartones,text="Se crearon "+str(ntryCantidadCartonesGetter)+" cartones", bg="#B0E0E6", font=("Finland", 10, 'bold'), fg ="#0EA928")
+                    lblInfoError.place(x=180,y=120)
             else:
-                label3.destroy()
-                label3=Label(generarCartonesPantalla,text="Debe de digitar un número entre 1 y 500.", bg="#B0E0E6", fg="black", font=("Finland", 10, 'bold'))
-                label3.place(x=150,y=40)
+                lblInfoError.destroy()
+                lblInfoError=Label(winGenerarCartones,text="El rango aceptado es de 1 a 500 cartones", bg="#B0E0E6", font=("Finland", 10, 'bold'), fg ="#C8420E")
+                lblInfoError.place(x=110,y=120)
         else:
-            label3.destroy()
-            label3=Label(generarCartonesPantalla,text="Digite un valor válido.", bg="#B0E0E6", fg="black", font=("Finland", 10, 'bold'))
-            label3.place(x=230,y=40)
+            lblInfoError.destroy()
+            lblInfoError=Label(winGenerarCartones,text="Valor inválido", bg="#B0E0E6", font=("Finland", 10, 'bold'), fg ="#C8420E")
+            lblInfoError.place(x=200,y=120)
     else:
-        label3.destroy()
-        label3=Label(generarCartonesPantalla,text="Ingrese el número de cartones a generar.", bg="#B0E0E6", fg="black", font=("Finland", 10, 'bold'))
-        label3.place(x=150,y=40)
+        lblInfoError.destroy()
+        lblInfoError=Label(winGenerarCartones,text="Ingrese un valor para continuar", bg="#B0E0E6", font=("Finland", 10, 'bold'), fg ="#C8420E")
+        lblInfoError.place(x=150,y=120)
+
 
          
 '''
@@ -66,9 +65,9 @@ Salidas: se borra el contenido de cantidadCartones (accion)
 Restriciones: event debe tener una interaccion con cantidadCartones para su uso (accion)
 '''
 #Eliminación del contenido de cantidadCartones
-def clickCantidadCartones(event):
-    cantidadCartones.config(state=NORMAL)
-    cantidadCartones.delete(0,END)
+#def clickCantidadCartones(event):
+    #ntryCantidadCartones.config(state=NORMAL)
+    #ntryCantidadCartones.delete(0,END)
 
     
 '''
@@ -77,11 +76,11 @@ Salidas: MP (interfaz)
 Restriciones: enviarCartonesPantalla (interfaz)
 '''
 #Abre la interfaz gráfica MP  
-def regresarFuncion():
-    global generarCartonesPantalla
-    generarCartonesPantalla.destroy()
+def goBackButton():
+    global winGenerarCartones
+    winGenerarCartones.destroy()
     MP.inicio()
-   
+
 
 '''
 Entradas: consultarCartonPantalla
@@ -89,46 +88,49 @@ Salidas: consultarCartonPantalla (interfaz)
 Restriciones: consultarCartonPantalla (interfaz)
 '''
 #Crea la interfaz gráfica 
-def inicio():
-    global generarCartonesPantalla
-    global cantidadCartones
-    global label3
+def interfaz():
+    global winGenerarCartones
+    global ntryCantidadCartones
+    global lblInfoError
 
     logic.eliminarRegistros()
     logic.eliminarCarpetaCartones()
-    generarCartonesPantalla=Tk()
-    generarCartonesPantalla.iconbitmap("bingo.ico")
-    generarCartonesPantalla.title("Generar Cartones de Bingo")
-    generarCartonesPantalla.config(bg="#B0E0E6")
-    generarCartonesPantalla.resizable(False, False)
-    window_width  = 600
-    window_height  = 130
-    screen_width  = generarCartonesPantalla.winfo_screenwidth()
-    screen_height  = generarCartonesPantalla.winfo_screenheight()
+    winGenerarCartones=Tk()
+    winGenerarCartones.iconbitmap("bingo.ico")
+    winGenerarCartones.title("Generar Cartones")
+    winGenerarCartones.config(bg="#B0E0E6")
+    winGenerarCartones.resizable(False, False)
+    window_width  = 510
+    window_height  = 400
+    
+    #Centrar interfaz en el centro de la botella 
+    screen_width  = winGenerarCartones.winfo_screenwidth()
+    screen_height  = winGenerarCartones.winfo_screenheight()
     position_top  = (screen_width /2) - (window_width /2)
     position_right  = (screen_height /2) - (window_height /2)
-    generarCartonesPantalla.geometry('%dx%d+%d+%d' % (window_width, window_height, position_top, position_right))
+    winGenerarCartones.geometry('%dx%d+%d+%d' % (window_width, window_height, position_top, position_right))
 
-    label2=Label(generarCartonesPantalla,text="", bg="#B0E0E6", fg="black", font=("Finland", 10))
+    label2=Label(winGenerarCartones,text="", bg="#B0E0E6", fg="black", font=("Finland", 10))
     label2.place(x=10,y=10)
 
-    label3=Label(generarCartonesPantalla,text="Ingrese la cantidad de cartones que desea generar:", bg="#B0E0E6", fg="black", font=("Finland", 10))
-    label3.place(x=20,y=10)
+
+    lblTitle=Label(winGenerarCartones,text="Ingrese la cantidad de cartones que desea generar", bg="#B0E0E6", fg="black", font=("Segoe UI bold", 13))
+    lblTitle.place(x=45,y=10)
 
     cantidadCartones_StringVar = StringVar()
-    cantidadCartones = Entry(generarCartonesPantalla, bg="white", fg="black", textvariable=cantidadCartones_StringVar, width="25")
-    cantidadCartones.insert(0,"Valor entre 1 y 500")
-    cantidadCartones.config(state=DISABLED)
-    cantidadCartones.bind("<Button-1>",clickCantidadCartones)
-    cantidadCartones.place(x=330,y=13)
 
-    botonIniciarSesion = Button(generarCartonesPantalla,text="Generar", command=generarFuncion, bg="#20B2AA", fg="black", font=("Finland", 10, 'bold'))
-    botonIniciarSesion.place(x=350,y=80)
+    ntryCantidadCartones = Entry(winGenerarCartones, bg="white", fg="black", textvariable=cantidadCartones_StringVar, width="25")
+    ntryCantidadCartones.config(width=65)
+    ntryCantidadCartones.place(x=50,y=45)
 
-    botonCerrarAplicacion = Button(generarCartonesPantalla,text="Menú Principal", command=regresarFuncion, bg="#E00000", fg="#FFFFFF", font=("Finland", 10, 'bold'))
-    botonCerrarAplicacion.place(x=150,y=80)
+    btnGenerar = Button(winGenerarCartones,text="Generar", command=generarFuncion, bg="#20B2AA", fg="black", font=("Finland", 11, 'bold'))
+    btnGenerar.config(width=35)
+    btnGenerar.place(x=80,y=80)
 
-    label3=Label(generarCartonesPantalla,text="", bg="#B0E0E6", fg="black", font=("Finland", 10))
-    label3.place(x=50,y=60)
+    btnVolver = Button(winGenerarCartones,text="             Volver             ", command=goBackButton, bg="#E00000", fg="#FFFFFF", font=("Finland", 11, 'bold'))
+    btnVolver.place(x=170,y=300)
 
-    generarCartonesPantalla.mainloop()
+    lblInfoError=Label(winGenerarCartones,text="", bg="#B0E0E6", fg="black", font=("Finland", 10))
+    lblInfoError.place(x=50,y=100)
+
+    winGenerarCartones.mainloop()
